2b2678c53ee28bfa52236f12de6887d280380ab0
Walid Bouguerra <153543876+walidbouguerra@users.noreply.github.com>
2023-12-20 10:13:15 +0100

    Ajout du router pour le controller

--- public/index.php
+++ public/index.php
@@ -1,9 +1,36 @@
 <?php
 // Récupération de l'url
 $url = htmlspecialchars($_GET['url'] ?? null);
+
 // Segmentation de l'url
 $url = explode('/', $url);
+
 // Initialisation des paramètres de l'url
 $controller = $url[0] ?? null;
 $action = $url[1] ?? null;
 $param = $url[2] ?? null;
+
+// Routage
+switch ($controller) {
+    case 'blog':
+        // $blogController = new blogController();
+        // $blogController->index();
+        echo 'Blog';
+        break;
+    case 'about':
+        // $aboutController = new AboutController();
+        // $aboutController->index();
+        echo 'About';
+        break;
+    case 'contact':
+        // $contactController = new contactController();
+        // $contactController->index();
+        echo 'Contact';
+        break;
+    
+    default:
+        // $accueilController = new accueilController();
+        // $accueilController->index();
+        echo 'Accueil';
+        break;
+}
